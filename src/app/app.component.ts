f37e4035bb39370559dfb68757fdc5b7d3e13c5a
HEMANT KHEDKAR <hemantkhedkar102034@gmail.com>
2023-04-04 17:04:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-data-table';
+  data:any ;
+  constructor(private http: HttpClient){
+   
+}
+
+ngOnInit(){
+  this.http.get('https://qaapi.jahernotice.com/api/Epp/0').subscribe((res:any) => {
+    this.data = res.data;
+  setTimeout(()=>{   
+    $('#datatableexample').DataTable( {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true,
+      lengthMenu : [5, 10, 25]
+  } );
+  }, 1);
+        }, error => console.error(error));
+}
+
+  
+
 }
